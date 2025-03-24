cc18f5b5a8749826dd1ed9dc8366d2090c29dcdd
Stef-Hermans <r1056664@student.thomasmore.be>
2025-03-24 18:50:43 +0100

    feat: storage done

--- topics/storage.starter.js
+++ topics/storage.starter.js
@@ -18,3 +18,52 @@
 // hint: you can use JSON.stringify to convert an array to string
 // hint: you can use JSON.parse to convert a string to array
 // hint: you can use appendChild to add a new li to the ul#movieList
+
+document.getElementById("saveName").addEventListener("click", function () {
+  let name = prompt("Enter your name:");
+  if (name) {
+    localStorage.setItem("myName", name);
+    document.getElementById("myName").textContent = name;
+  }
+});
+
+document.getElementById("showName").addEventListener("click", function () {
+  let storedName = localStorage.getItem("myName");
+  document.getElementById("myName").textContent = storedName
+    ? storedName
+    : "No name stored";
+});
+
+document.getElementById("removeName").addEventListener("click", function () {
+  localStorage.removeItem("myName");
+  document.getElementById("myName").textContent = "";
+});
+
+document.getElementById("addMovie").addEventListener("click", function () {
+  let movie = prompt("Enter a movie name:");
+  if (movie) {
+    let movies = JSON.parse(localStorage.getItem("movies")) || [];
+    movies.push(movie);
+    localStorage.setItem("movies", JSON.stringify(movies));
+    updateMovieList();
+  }
+});
+
+function updateMovieList() {
+  let movieList = document.getElementById("movieList");
+  movieList.innerHTML = "";
+  let movies = JSON.parse(localStorage.getItem("movies")) || [];
+  movies.forEach((movie) => {
+    let li = document.createElement("li");
+    li.textContent = movie;
+    movieList.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("myName")) {
+    document.getElementById("myName").textContent =
+      localStorage.getItem("myName");
+  }
+  updateMovieList();
+});
